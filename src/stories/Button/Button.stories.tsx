12e0dbfbf899b161acc83dc8c9af1f6f861e760e
gyutato <gyuwonlee.olivia@gmail.com>
2022-12-30 21:38:00 +0900

    feat: modify argTypes for children prop

--- src/stories/Button/Button.stories.tsx
+++ src/stories/Button/Button.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Default from "./Button.component";
+import { Typography } from "@mui/material";
 
 export const Basic: StoryObj = {
   args: {
@@ -11,4 +12,14 @@ export const Basic: StoryObj = {
 export default {
   title: "Components/Button",
   component: Default,
+  argTypes: {
+    children: {
+      control: { type: "radio" },
+      options: ["text", "Typography"],
+      mapping: {
+        text: "Button",
+        Typography: <Typography>Button</Typography>,
+      },
+    },
+  },
 } as Meta;
